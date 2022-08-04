99d11ac9969a5e06ab67e7f91f5edc5f2344b0f1
Anderson <andersonby@163.com>
2022-08-05 04:04:00 +0800

    update

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import sys, os
+# -*- coding: utf-8 -*-
+# @Author: Bi Ying
+# @Date:   2022-08-02 20:03:15
+# @Last Modified by:   Bi Ying
+# @Last Modified time: 2022-08-05 03:54:20
+import sys
+import os
 
-parent_folder_path = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(parent_folder_path)
-sys.path.append(os.path.join(parent_folder_path, "lib"))
-sys.path.append(os.path.join(parent_folder_path, "plugin"))
+plugindir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(plugindir)
+sys.path.append(os.path.join(plugindir, "lib"))
+sys.path.append(os.path.join(plugindir, "plugin"))
 
 # -*- coding: utf-8 -*-
 
